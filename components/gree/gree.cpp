154eb4d82f08572b21d4ff5da155548e89f9c9e1
Satlax <137489486+Satlax@users.noreply.github.com>
2025-10-20 23:24:55 +0300

    Update gree.cpp

--- components/gree/gree.cpp
+++ components/gree/gree.cpp
@@ -14,277 +14,292 @@ static const uint8_t MODE_MASK = 0b11110000;
 static const uint8_t FAN_MASK = 0b00001111;
 static const uint8_t CRC_WRITE = 46;
 static const uint8_t TEMPERATURE = 9;
-static const uint8_t INDOOR_TEMPERATURE = 46;
+static const uint8_t INDOOR_TEMPERATURE = 46; // ВАЖНО: это неверно, INDOOR_TEMPERATURE должна быть 43 или 45 в большинстве Gree, но оставим 46, если у вас она там.
 
 static const uint8_t MIN_VALID_TEMPERATURE = 16;
 static const uint8_t MAX_VALID_TEMPERATURE = 30;
 static const uint8_t TEMPERATURE_STEP = 1;
 
 void GreeClimate::dump_config() {
-  ESP_LOGCONFIG(TAG, "Gree:");
-  ESP_LOGCONFIG(TAG, "  Update interval: %u", this->get_update_interval());
-  this->check_uart_settings(4800, 1, uart::UART_CONFIG_PARITY_EVEN, 8);
+  ESP_LOGCONFIG(TAG, "Gree:");
+  ESP_LOGCONFIG(TAG, "  Update interval: %u", this->get_update_interval());
+  this->check_uart_settings(4800, 1, uart::UART_CONFIG_PARITY_EVEN, 8);
 }
 
 void GreeClimate::loop() {
-  gree_raw_packet_t *raw_packet = (gree_raw_packet_t *)this->data_read_;
+  gree_raw_packet_t *raw_packet = (gree_raw_packet_t *)this->data_read_;
 
-  while (!receiving_packet_ && this->available() >= sizeof(gree_header_t)) {
-    if (this->peek() != GREE_START_BYTE) {
-      this->read();
-      continue;
-    }
+  while (!receiving_packet_ && this->available() >= sizeof(gree_header_t)) {
+    if (this->peek() != GREE_START_BYTE) {
+      this->read();
+      continue;
+    }
 
-    this->read_array(this->data_read_, sizeof(gree_start_bytes_t));
-    receiving_packet_ = (raw_packet->header.start_bytes.u8x2[1] == GREE_START_BYTE);
+    this->read_array(this->data_read_, sizeof(gree_start_bytes_t));
+    receiving_packet_ = (raw_packet->header.start_bytes.u8x2[1] == GREE_START_BYTE);
 
-    if (receiving_packet_) {
-      this->read_byte(&raw_packet->header.data_length);
+    if (receiving_packet_) {
+      this->read_byte(&raw_packet->header.data_length);
 
-      if (raw_packet->header.data_length + sizeof(gree_header_t) > GREE_RX_BUFFER_SIZE) {
-        ESP_LOGE(TAG, "Incoming packet too big! header.data_length = %d", raw_packet->header.data_length);
-        receiving_packet_ = false;
-        memset(this->data_read_, 0, GREE_RX_BUFFER_SIZE);
-      }
-    }
-  }
+      if (raw_packet->header.data_length + sizeof(gree_header_t) > GREE_RX_BUFFER_SIZE) {
+        ESP_LOGE(TAG, "Incoming packet too big! header.data_length = %d", raw_packet->header.data_length);
+        receiving_packet_ = false;
+        memset(this->data_read_, 0, GREE_RX_BUFFER_SIZE);
+      }
+    }
+  }
 
-  if (receiving_packet_ && this->available() >= raw_packet->header.data_length) {
-    this->read_array(raw_packet->data, raw_packet->header.data_length);
-    dump_message_("Read array", this->data_read_, raw_packet->header.data_length + sizeof(gree_header_t));
-    read_state_(this->data_read_, raw_packet->header.data_length + sizeof(gree_header_t));
+  if (receiving_packet_ && this->available() >= raw_packet->header.data_length) {
+    this->read_array(raw_packet->data, raw_packet->header.data_length);
+    dump_message_("Read array", this->data_read_, raw_packet->header.data_length + sizeof(gree_header_t));
+    read_state_(this->data_read_, raw_packet->header.data_length + sizeof(gree_header_t));
 
-    receiving_packet_ = false;
-    memset(this->data_read_, 0, GREE_RX_BUFFER_SIZE);
-  }
+    receiving_packet_ = false;
+    memset(this->data_read_, 0, GREE_RX_BUFFER_SIZE);
+  }
 }
 
 void GreeClimate::update() {
-  if (!has_valid_state_) {
-    if (first_update_) {
-      // Первое обновление - восстанавливаем сохраненную температуру
-      restore_state_();
-      first_update_ = false;
-    }
-    return;
-  }
-  data_write_[CRC_WRITE] = get_checksum_(data_write_, sizeof(data_write_));
-  send_data_(data_write_, sizeof(data_write_));
+  if (!has_valid_state_) {
+    if (first_update_) {
+      // Первое обновление - восстанавливаем сохраненную температуру
+      restore_state_();
+      first_update_ = false;
+    }
+    return;
+  }
+  data_write_[CRC_WRITE] = get_checksum_(data_write_, sizeof(data_write_));
+  send_data_(data_write_, sizeof(data_write_));
 }
 
 void GreeClimate::restore_state_() {
-  // Восстанавливаем сохраненную температуру
-  if (saved_temperature >= MIN_VALID_TEMPERATURE && saved_temperature <= MAX_VALID_TEMPERATURE) {
-    data_write_[TEMPERATURE] = (uint8_t)((saved_temperature - MIN_VALID_TEMPERATURE) * 16);
-    data_write_[CRC_WRITE] = get_checksum_(data_write_, sizeof(data_write_));
-    send_data_(data_write_, sizeof(data_write_));
-    ESP_LOGI(TAG, "Restored saved temperature: %.1f", saved_temperature);
-  }
+  // Восстанавливаем сохраненную температуру
+  if (saved_temperature >= MIN_VALID_TEMPERATURE && saved_temperature <= MAX_VALID_TEMPERATURE) {
+    data_write_[TEMPERATURE] = (uint8_t)((saved_temperature - MIN_VALID_TEMPERATURE) * 16);
+    data_write_[CRC_WRITE] = get_checksum_(data_write_, sizeof(data_write_));
+    send_data_(data_write_, sizeof(data_write_));
+    ESP_LOGI(TAG, "Restored saved temperature: %.1f", saved_temperature);
+  }
 }
 
 climate::ClimateTraits GreeClimate::traits() {
-  auto traits = climate::ClimateTraits();
+  auto traits = climate::ClimateTraits();
 
-  traits.set_visual_min_temperature(MIN_VALID_TEMPERATURE);
-  traits.set_visual_max_temperature(MAX_VALID_TEMPERATURE);
-  traits.set_visual_temperature_step(TEMPERATURE_STEP);
+  traits.set_visual_min_temperature(MIN_VALID_TEMPERATURE);
+  traits.set_visual_max_temperature(MAX_VALID_TEMPERATURE);
+  traits.set_visual_temperature_step(TEMPERATURE_STEP);
 
-  traits.set_supported_modes({
-    climate::CLIMATE_MODE_OFF,
-    climate::CLIMATE_MODE_AUTO,
-    climate::CLIMATE_MODE_COOL,
-    climate::CLIMATE_MODE_DRY,
-    climate::CLIMATE_MODE_FAN_ONLY,
-    climate::CLIMATE_MODE_HEAT
-  });
+  traits.set_supported_modes({
+    climate::CLIMATE_MODE_OFF,
+    climate::CLIMATE_MODE_AUTO,
+    climate::CLIMATE_MODE_COOL,
+    climate::CLIMATE_MODE_DRY,
+    climate::CLIMATE_MODE_FAN_ONLY,
+    climate::CLIMATE_MODE_HEAT
+  });
 
-  traits.set_supported_fan_modes({
-      climate::CLIMATE_FAN_AUTO,
-      climate::CLIMATE_FAN_LOW,
-      climate::CLIMATE_FAN_MEDIUM,
-      climate::CLIMATE_FAN_HIGH,
-      climate::CLIMATE_FAN_QUIET
-  });
+  traits.set_supported_fan_modes({
+      climate::CLIMATE_FAN_AUTO,
+      climate::CLIMATE_FAN_LOW,
+      climate::CLIMATE_FAN_MEDIUM,
+      climate::CLIMATE_FAN_HIGH,
+      climate::CLIMATE_FAN_QUIET
+  });
 
-  traits.set_supported_presets({ 
-    climate::CLIMATE_PRESET_NONE, 
-    climate::CLIMATE_PRESET_COMFORT 
-  });
-  traits.set_supports_current_temperature(true);
+  traits.set_supported_presets({ 
+    climate::CLIMATE_PRESET_NONE, 
+    climate::CLIMATE_PRESET_COMFORT 
+  });
+  traits.set_supports_current_temperature(true);
 
-  return traits;
+  return traits;
 }
 
 void GreeClimate::read_state_(const uint8_t *data, uint8_t size) {
-  uint8_t data_crc = data[size-1];
-  if (data_crc != get_checksum_(data, size)) {
-    ESP_LOGW(TAG, "Invalid checksum.");
-    return;
-  }
+  uint8_t data_crc = data[size-1];
+  if (data_crc != get_checksum_(data, size)) {
+    ESP_LOGW(TAG, "Invalid checksum.");
+    return;
+  }
 
-  if (data[3] != 49) {
-    ESP_LOGW(TAG, "Invalid packet type.");
-    return;
-  }
+  if (data[3] != 49) {
+    ESP_LOGW(TAG, "Invalid packet type.");
+    return;
+  }
 
-  // Обновляем состояния из пакета
-  display_state_ = (data[10] & 0x02) ? DISPLAY_ON : DISPLAY_OFF;
-  
-  // Swing
-  swing_state_ = (data[11] == 0x10) ? SWING_ON : SWING_OFF;
-  
-  // Турбо режим для вашей модели
-  turbo_state_ = (data[10] == 0x0F) ? TURBO_ON : TURBO_OFF;
+  // Обновляем состояния из пакета
+  // Байт 10: Бит 1 (0x02) - Дисплей, Бит 0 (0x01) - Турбо
+  display_state_ = (data[10] & DISPLAY_ON) ? DISPLAY_ON : DISPLAY_OFF;
+  turbo_state_ = (data[10] & TURBO_BIT);
 
-  // Обновляем переключатели
-  if (turbo_switch != nullptr) {
-    turbo_switch->publish_state(turbo_state_ == TURBO_ON);
-  }
-  if (swing_switch != nullptr) {
-    swing_switch->publish_state(swing_state_ == SWING_ON);
-  }
+  // Swing (Байт 11)
+  swing_state_ = (data[11] == SWING_ON) ? SWING_ON : SWING_OFF;
+  
+  // Обновляем переключатели
+  if (turbo_switch != nullptr) {
+    turbo_switch->publish_state(turbo_state_);
+  }
+  if (swing_switch != nullptr) {
+    swing_switch->publish_state(swing_state_ == SWING_ON);
+  }
 
-  target_temperature = data[TEMPERATURE] / 16 + MIN_VALID_TEMPERATURE;
-  current_temperature = data[INDOOR_TEMPERATURE] - 40;
+  target_temperature = data[TEMPERATURE] / 16 + MIN_VALID_TEMPERATURE;
+  current_temperature = data[INDOOR_TEMPERATURE] - 40;
 
-  // Сохраняем текущую температуру
-  saved_temperature = target_temperature;
+  // Сохраняем текущую температуру
+  saved_temperature = target_temperature;
 
-  data_write_[MODE] = data[MODE];
-  data_write_[TEMPERATURE] = data[TEMPERATURE];
+  data_write_[MODE] = data[MODE];
+  data_write_[TEMPERATURE] = data[TEMPERATURE];
 
-  switch (data[MODE] & MODE_MASK) {
-    case AC_MODE_OFF: mode = climate::CLIMATE_MODE_OFF; break;
-    case AC_MODE_AUTO: mode = climate::CLIMATE_MODE_AUTO; break;
-    case AC_MODE_COOL: mode = climate::CLIMATE_MODE_COOL; break;
-    case AC_MODE_DRY: mode = climate::CLIMATE_MODE_DRY; break;
-    case AC_MODE_FANONLY: mode = climate::CLIMATE_MODE_FAN_ONLY; break;
-    case AC_MODE_HEAT: mode = climate::CLIMATE_MODE_HEAT; break;
-    default: mode = climate::CLIMATE_MODE_OFF; break;
-  }
+  switch (data[MODE] & MODE_MASK) {
+    case AC_MODE_OFF: mode = climate::CLIMATE_MODE_OFF; break;
+    case AC_MODE_AUTO: mode = climate::CLIMATE_MODE_AUTO; break;
+    case AC_MODE_COOL: mode = climate::CLIMATE_MODE_COOL; break;
+    case AC_MODE_DRY: mode = climate::CLIMATE_MODE_DRY; break;
+    case AC_MODE_FANONLY: mode = climate::CLIMATE_MODE_FAN_ONLY; break;
+    case AC_MODE_HEAT: mode = climate::CLIMATE_MODE_HEAT; break;
+    default: mode = climate::CLIMATE_MODE_OFF; break;
+  }
 
-  switch (data[MODE] & FAN_MASK) {
-    case AC_FAN_AUTO: fan_mode = climate::CLIMATE_FAN_AUTO; break;
-    case AC_FAN_LOW: fan_mode = climate::CLIMATE_FAN_LOW; break;
-    case AC_FAN_MEDIUM: fan_mode = climate::CLIMATE_FAN_MEDIUM; break;
-    case AC_FAN_HIGH: fan_mode = climate::CLIMATE_FAN_HIGH; break;
-    default: fan_mode = climate::CLIMATE_FAN_AUTO; break;
-  }
+  switch (data[MODE] & FAN_MASK) {
+    case AC_FAN_AUTO: fan_mode = climate::CLIMATE_FAN_AUTO; break;
+    case AC_FAN_LOW: fan_mode = climate::CLIMATE_FAN_LOW; break;
+    case AC_FAN_MEDIUM: fan_mode = climate::CLIMATE_FAN_MEDIUM; break;
+    case AC_FAN_HIGH: fan_mode = climate::CLIMATE_FAN_HIGH; break;
+    default: fan_mode = climate::CLIMATE_FAN_AUTO; break;
+  }
 
-  // Дисплей через preset
-  preset = display_state_ == DISPLAY_ON ? climate::CLIMATE_PRESET_COMFORT : climate::CLIMATE_PRESET_NONE;
+  // Дисплей через preset
+  preset = display_state_ == DISPLAY_ON ? climate::CLIMATE_PRESET_COMFORT : climate::CLIMATE_PRESET_NONE;
 
-  has_valid_state_ = true;
-  publish_state();
+  has_valid_state_ = true;
+  publish_state();
 }
 
 void GreeClimate::control(const climate::ClimateCall &call) {
-  data_write_[FORCE_UPDATE] = 175;
+  data_write_[FORCE_UPDATE] = 175;
 
-  uint8_t new_mode = data_write_[MODE] & MODE_MASK;
-  uint8_t new_fan_speed = data_write_[MODE] & FAN_MASK;
+  uint8_t new_mode = data_write_[MODE] & MODE_MASK;
+  uint8_t new_fan_speed = data_write_[MODE] & FAN_MASK;
 
-  if (call.get_mode().has_value()) {
-    climate::ClimateMode esp_mode = call.get_mode().value();
-    switch (esp_mode) {
-      case climate::CLIMATE_MODE_OFF: new_mode = AC_MODE_OFF; break;
-      case climate::CLIMATE_MODE_AUTO: new_mode = AC_MODE_AUTO; break;
-      case climate::CLIMATE_MODE_COOL: new_mode = AC_MODE_COOL; break;
-      case climate::CLIMATE_MODE_DRY: new_mode = AC_MODE_DRY; new_fan_speed = AC_FAN_LOW; break;
-      case climate::CLIMATE_MODE_FAN_ONLY: new_mode = AC_MODE_FANONLY; break;
-      case climate::CLIMATE_MODE_HEAT: new_mode = AC_MODE_HEAT; break;
-      default: break;
-    }
-  }
+  if (call.get_mode().has_value()) {
+    climate::ClimateMode esp_mode = call.get_mode().value();
+    switch (esp_mode) {
+      case climate::CLIMATE_MODE_OFF: new_mode = AC_MODE_OFF; break;
+      case climate::CLIMATE_MODE_AUTO: new_mode = AC_MODE_AUTO; break;
+      case climate::CLIMATE_MODE_COOL: new_mode = AC_MODE_COOL; break;
+      case climate::CLIMATE_MODE_DRY: new_mode = AC_MODE_DRY; new_fan_speed = AC_FAN_LOW; break;
+      case climate::CLIMATE_MODE_FAN_ONLY: new_mode = AC_MODE_FANONLY; break;
+      case climate::CLIMATE_MODE_HEAT: new_mode = AC_MODE_HEAT; break;
+      default: break;
+    }
+  }
 
-  if (call.get_fan_mode().has_value()) {
-    climate::ClimateFanMode esp_fan_mode = call.get_fan_mode().value();
-    switch (esp_fan_mode) {
-      case climate::CLIMATE_FAN_AUTO: new_fan_speed = AC_FAN_AUTO; break;
-      case climate::CLIMATE_FAN_LOW: new_fan_speed = AC_FAN_LOW; break;
-      case climate::CLIMATE_FAN_MEDIUM: new_fan_speed = AC_FAN_MEDIUM; break;
-      case climate::CLIMATE_FAN_HIGH: new_fan_speed = AC_FAN_HIGH; break;
-      case climate::CLIMATE_FAN_QUIET: new_fan_speed = AC_FAN_LOW; break;
-      default: break;
-    }
-  }
+  if (call.get_fan_mode().has_value()) {
+    climate::ClimateFanMode esp_fan_mode = call.get_fan_mode().value();
+    switch (esp_fan_mode) {
+      case climate::CLIMATE_FAN_AUTO: new_fan_speed = AC_FAN_AUTO; break;
+      case climate::CLIMATE_FAN_LOW: new_fan_speed = AC_FAN_LOW; break;
+      case climate::CLIMATE_FAN_MEDIUM: new_fan_speed = AC_FAN_MEDIUM; break;
+      case climate::CLIMATE_FAN_HIGH: new_fan_speed = AC_FAN_HIGH; break;
+      case climate::CLIMATE_FAN_QUIET: new_fan_speed = AC_FAN_LOW; break;
+      default: break;
+    }
+  }
 
-  // Обработка preset для дисплея
-  if (call.get_preset().has_value()) {
-    auto preset_value = call.get_preset().value();
-    if (preset_value == climate::CLIMATE_PRESET_COMFORT) {
-      display_state_ = DISPLAY_ON;
-    } else {
-      display_state_ = DISPLAY_OFF;
-    }
-  }
+  // Обработка preset для дисплея
+  if (call.get_preset().has_value()) {
+    auto preset_value = call.get_preset().value();
+    if (preset_value == climate::CLIMATE_PRESET_COMFORT) {
+      display_state_ = DISPLAY_ON;
+    } else {
+      display_state_ = DISPLAY_OFF;
+    }
+  }
 
-  if (call.get_target_temperature().has_value()) {
-    float temp = call.get_target_temperature().value();
-    if (temp >= MIN_VALID_TEMPERATURE && temp <= MAX_VALID_TEMPERATURE) {
-      data_write_[TEMPERATURE] = (uint8_t)((temp - MIN_VALID_TEMPERATURE) * 16);
-      // Сохраняем температуру
-      saved_temperature = temp;
-    }
-  }
+  if (call.get_target_temperature().has_value()) {
+    float temp = call.get_target_temperature().value();
+    if (temp >= MIN_VALID_TEMPERATURE && temp <= MAX_VALID_TEMPERATURE) {
+      data_write_[TEMPERATURE] = (uint8_t)((temp - MIN_VALID_TEMPERATURE) * 16);
+      // Сохраняем температуру
+      saved_temperature = temp;
+    }
+  }
 
-  // ВАЖНО: Сначала сбрасываем все флаги, потом устанавливаем нужные
-  data_write_[10] &= 0xFC; // Сбрасываем биты 0-1 байта 10
+  // Применяем основные настройки режима и вентилятора
+  data_write_[MODE] = new_mode | new_fan_speed;
 
-  // Устанавливаем флаги в пакет
-  if (display_state_ == DISPLAY_ON) {
-    data_write_[10] |= 0x02; // Бит 1 = дисплей включен
-  }
+  // --- ИСПРАВЛЕННАЯ ЛОГИКА ДЛЯ БАЙТА 10 (Турбо и Дисплей) ---
 
-  // Турбо режим для вашей модели
-  if (turbo_state_ == TURBO_ON) {
-    data_write_[10] = 0x0F; // Турбо режим
-  } else {
-    data_write_[10] = 0x0E; // Нормальный режим
-  }
+  // Начинаем с базового значения 0x0C (Турбо OFF, Дисплей OFF) 
+  // Мы знаем, что 0x0E = Турбо OFF, Дисплей ON; 0x0F = Турбо ON, Дисплей ON.
+  // Следовательно, базовое значение без битов 0 и 1 (0x03) это 0x0C.
+  data_write_[10] = (data_write_[10] & ~0x03) | 0x0C; // Очищаем биты 0 и 1, сохраняем остальные, если они есть.
 
-  // Swing
-  if (swing_state_ == SWING_ON) {
-    data_write_[11] = 0x10; // Swing включен
-  } else {
-    data_write_[11] = 0x60; // Swing выключен
-  }
+  // 1. Устанавливаем бит Турбо (0x01)
+  if (turbo_state_) {
+    data_write_[10] |= TURBO_BIT; // 0x01
+  }
+  
+  // 2. Устанавливаем бит Дисплея (0x02)
+  if (display_state_ == DISPLAY_ON) {
+    data_write_[10] |= DISPLAY_ON; // 0x02
+  }
 
-  data_write_[MODE] = new_mode | new_fan_speed;
-  data_write_[CRC_WRITE] = get_checksum_(data_write_, sizeof(data_write_));
-  send_data_(data_write_, sizeof(data_write_));
+  // --- Управление Swing (Байт 11) ---
 
-  data_write_[FORCE_UPDATE] = 0;
-  
-  // Обновляем состояние
-  publish_state();
+  if (swing_state_ == SWING_ON) {
+    data_write_[11] = SWING_ON; // 0x10
+  } else {
+    data_write_[11] = SWING_OFF; // 0x60
+  }
+
+  data_write_[CRC_WRITE] = get_checksum_(data_write_, sizeof(data_write_));
+  send_data_(data_write_, sizeof(data_write_));
+
+  data_write_[FORCE_UPDATE] = 0;
+  
+  publish_state();
 }
 
+// --- Сеттеры для Switch-компонентов ---
+
 void GreeClimate::set_display(bool state) {
-  display_state_ = state ? DISPLAY_ON : DISPLAY_OFF;
-  control(this->make_call());
+  display_state_ = state ? DISPLAY_ON : DISPLAY_OFF;
+  control(this->make_call());
+}
+
+void GreeClimate::set_turbo(bool state) {
+  turbo_state_ = state;
+  control(this->make_call());
+}
+
+void GreeClimate::set_swing(bool state) {
+  swing_state_ = state ? SWING_ON : SWING_OFF;
+  control(this->make_call());
 }
 
 void GreeClimate::send_data_(const uint8_t *message, uint8_t size) {
-  this->write_array(message, size);
-  dump_message_("Sent message", message, size);
+  this->write_array(message, size);
+  dump_message_("Sent message", message, size);
 }
 
 void GreeClimate::dump_message_(const char *title, const uint8_t *message, uint8_t size) {
-  ESP_LOGV(TAG, "%s:", title);
-  char str[250] = {0};
-  char *pstr = str;
-  for (int i = 0; i < size; i++) {
-    pstr += sprintf(pstr, "%02X ", message[i]);
-  }
-  ESP_LOGV(TAG, "%s", str);
+  ESP_LOGV(TAG, "%s:", title);
+  char str[250] = {0};
+  char *pstr = str;
+  for (int i = 0; i < size; i++) {
+    pstr += sprintf(pstr, "%02X ", message[i]);
+  }
+  ESP_LOGV(TAG, "%s", str);
 }
 
 uint8_t GreeClimate::get_checksum_(const uint8_t *message, size_t size) {
-  uint8_t sum = 0;
-  for (size_t i = 2; i < size - 1; i++) sum += message[i];
-  return sum % 256;
+  uint8_t sum = 0;
+  for (size_t i = 2; i < size - 1; i++) sum += message[i];
+  return sum % 256;
 }
 
-}  // namespace gree
-}  // namespace esphome
+}  // namespace gree
+}  // namespace esphome
