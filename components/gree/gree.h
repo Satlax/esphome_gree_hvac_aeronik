aa07bd96f0d8e47787838a893684de2a3fd134ca
Satlax <137489486+Satlax@users.noreply.github.com>
2025-10-20 18:57:31 +0300

    Update gree.h

--- components/gree/gree.h
+++ components/gree/gree.h
@@ -1,63 +1,85 @@
 #pragma once
 
-#include "esphome.h"
+#include "esphome/core/component.h"
+#include "esphome/components/climate/climate.h"
+#include "esphome/components/uart/uart.h"
+#include "esphome/core/log.h"
 
 namespace esphome {
 namespace gree {
 
-class GreeClimate : public climate::Climate, public Component {
+enum ac_mode: uint8_t {
+  AC_MODE_OFF = 0x10,
+  AC_MODE_AUTO = 0x80,
+  AC_MODE_COOL = 0x90,
+  AC_MODE_DRY = 0xA0,
+  AC_MODE_FANONLY = 0xB0,
+  AC_MODE_HEAT = 0xC0
+};
+
+enum ac_fan: uint8_t {
+  AC_FAN_AUTO = 0x00,
+  AC_FAN_LOW = 0x01,
+  AC_FAN_MEDIUM = 0x02,
+  AC_FAN_HIGH = 0x03
+};
+
+#define GREE_START_BYTE 0x7E
+#define GREE_RX_BUFFER_SIZE 52
+
+union gree_start_bytes_t {
+    uint8_t u8x2[2];
+};
+
+struct gree_header_t {
+  gree_start_bytes_t start_bytes;
+  uint8_t data_length;
+};
+
+struct gree_raw_packet_t {
+  gree_header_t header;
+  uint8_t data[1]; // first data byte
+};
+
+class GreeClimate : public climate::Climate, public uart::UARTDevice, public PollingComponent {
  public:
-  void setup() override {
-    // Инициализация
+  void loop() override;
+  void update() override;
+  void dump_config() override;
+  void control(const climate::ClimateCall &call) override;
+
+  void set_display_light(bool state);
+  bool is_display_light_on() const { return display_light_state_; }
+
+  void set_silent_mode(bool state);
+  bool is_silent_mode_on() const { return silent_mode_; }
+
+  void set_supported_presets(const std::set<climate::ClimatePreset> &presets) {
+    this->supported_presets_ = presets;
   }
 
-  void control(const climate::ClimateCall &call) override {
-    if (call.get_mode().has_value()) {
-      // Управление режимом
-    }
-    if (call.get_fan_mode().has_value()) {
-      // Управление скоростью вентилятора
-    }
-    if (call.get_preset().has_value()) {
-      // Управление пресетами
-      handle_preset(call.get_preset().value());
-    }
-  }
-
-  void handle_preset(climate::ClimatePreset preset) {
-    switch (preset) {
-      case climate::CLIMATE_PRESET_BOOST:
-        // Включение Turbo Mode
-        break;
-      case climate::CLIMATE_PRESET_SLEEP:
-        // Включение Silent Mode
-        break;
-      case climate::CLIMATE_PRESET_CUSTOM_1:
-        // Включение Display Light
-        break;
-      default:
-        break;
-    }
-  }
-
-  climate::ClimateTraits traits() override {
-    auto traits = climate::ClimateTraits();
-    traits.set_supports_modes({climate::CLIMATE_MODE_OFF, climate::CLIMATE_MODE_HEAT, climate::CLIMATE_MODE_COOL});
-    traits.set_supports_presets({
-      climate::CLIMATE_PRESET_BOOST,
-      climate::CLIMATE_PRESET_SLEEP,
-      climate::CLIMATE_PRESET_CUSTOM_1,
-    });
-    return traits;
-  }
-
-  void dump_config() override {
-    ESP_LOGCONFIG(TAG, "Gree Climate:");
-    // Вывод конфигурации
-  }
+ protected:
+  climate::ClimateTraits traits() override;
+  void read_state_(const uint8_t *data, uint8_t size);
+  void send_data_(const uint8_t *message, uint8_t size);
+  void dump_message_(const char *title, const uint8_t *message, uint8_t size);
+  uint8_t get_checksum_(const uint8_t *message, size_t size);
 
  private:
-  static const char *TAG;
+  uint8_t data_write_[47] = {0x7E, 0x7E, 0x2C, 0x01, 0x00, 0x00, 0x00, 0x00,
+                             0x00, 0x00, 0x02, 0x02, 0x00, 0x00, 0x00, 0x00,
+                             0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                             0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                             0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                             0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+  uint8_t data_read_[GREE_RX_BUFFER_SIZE] = {0};
+
+  bool receiving_packet_ = false;
+  bool has_valid_state_ = false;
+  bool display_light_state_ = false;
+  bool silent_mode_ = false;
+
+  std::set<climate::ClimatePreset> supported_presets_{};
 };
 
 }  // namespace gree
