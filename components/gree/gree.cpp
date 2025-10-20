8cd7e1706956f76ffd6ad2a62022181548688115
Satlax <137489486+Satlax@users.noreply.github.com>
2025-10-20 14:52:02 +0300

    Update gree.cpp

--- components/gree/gree.cpp
+++ components/gree/gree.cpp
@@ -8,9 +8,6 @@ namespace gree {
 static const char *const TAG = "gree";
 
 // block of byte positions in requests/answers
-// GrKoR: I recommend change this approach (byte positions) to structures like gree_raw_packet_t.
-// You can use pointers (and type casting) for assignment structure to the data buffer.
-// It will be much much easier to work with structures instead of array and byte positions
 static const uint8_t FORCE_UPDATE = 7;
 static const uint8_t MODE = 8;
 static const uint8_t MODE_MASK = 0b11110000;
@@ -18,9 +15,6 @@ static const uint8_t FAN_MASK = 0b00001111;
 static const uint8_t SWING = 12;
 
 static const uint8_t CRC_WRITE = 46;
-//CRC_READ moved to read_state_ with last bytes because of different length of incoming packets
-//static const uint8_t CRC_READ = 49;
-
 static const uint8_t TEMPERATURE = 9;
 static const uint8_t INDOOR_TEMPERATURE = 46;
 
@@ -29,7 +23,6 @@ static const uint8_t MIN_VALID_TEMPERATURE = 16;
 static const uint8_t MAX_VALID_TEMPERATURE = 30;
 static const uint8_t TEMPERATURE_STEP = 1;
 
-// prints user configuration
 void GreeClimate::dump_config() {
   ESP_LOGCONFIG(TAG, "Gree:");
   ESP_LOGCONFIG(TAG, "  Update interval: %u", this->get_update_interval());
@@ -37,23 +30,29 @@ void GreeClimate::dump_config() {
   this->check_uart_settings(4800, 1, uart::UART_CONFIG_PARITY_EVEN, 8);
 }
 
+void GreeClimate::setup() {
+  ESP_LOGI(TAG, "Sending initial handshake to AC...");
+  send_data_(data_write_, sizeof(data_write_));
+}
+
 void GreeClimate::loop() {
   gree_raw_packet_t *raw_packet = (gree_raw_packet_t *)this->data_read_;
 
   while (!receiving_packet_ && this->available() >= sizeof(gree_header_t)) {
     if (this->peek() != GREE_START_BYTE) {
-      this->read(); // читаем байт "в никуда"
+      this->read();
       continue;
     }
 
     this->read_array(this->data_read_, sizeof(gree_start_bytes_t));
     receiving_packet_ = (raw_packet->header.start_bytes.u8x2[1] == GREE_START_BYTE);
-    
+
     if (receiving_packet_) {
-      this->read_byte( &raw_packet->header.data_length );
+      this->read_byte(&raw_packet->header.data_length);
 
       if (raw_packet->header.data_length + sizeof(gree_header_t) > GREE_RX_BUFFER_SIZE) {
-        ESP_LOGE(TAG, "Incoming packet is too big! header.data_length = %d, maximum is %d", raw_packet->header.data_length, GREE_RX_BUFFER_SIZE - sizeof(gree_header_t));
+        ESP_LOGE(TAG, "Incoming packet too big! header.data_length = %d, max = %d",
+                 raw_packet->header.data_length, GREE_RX_BUFFER_SIZE - sizeof(gree_header_t));
         receiving_packet_ = false;
         memset(this->data_read_, 0, GREE_RX_BUFFER_SIZE);
       }
@@ -62,29 +61,25 @@ void GreeClimate::loop() {
 
   if (receiving_packet_ && this->available() >= raw_packet->header.data_length) {
     this->read_array(raw_packet->data, raw_packet->header.data_length);
-
     dump_message_("Read array", this->data_read_, raw_packet->header.data_length + sizeof(gree_header_t));
     read_state_(this->data_read_, raw_packet->header.data_length + sizeof(gree_header_t));
-    
+
     receiving_packet_ = false;
     memset(this->data_read_, 0, GREE_RX_BUFFER_SIZE);
   }
 }
 
-/*
-void GreeClimate::setup() {
-  this->set_update_interval(300);
-}
-*/
-
 void GreeClimate::update() {
+  if (!has_valid_state_) {
+    ESP_LOGW(TAG, "Skipping update: no valid state from AC yet");
+    return;
+  }
   data_write_[CRC_WRITE] = get_checksum_(data_write_, sizeof(data_write_));
   send_data_(data_write_, sizeof(data_write_));
 }
 
 climate::ClimateTraits GreeClimate::traits() {
   auto traits = climate::ClimateTraits();
-
   traits.set_visual_min_temperature(MIN_VALID_TEMPERATURE);
   traits.set_visual_max_temperature(MAX_VALID_TEMPERATURE);
   traits.set_visual_temperature_step(TEMPERATURE_STEP);
@@ -99,268 +94,139 @@ climate::ClimateTraits GreeClimate::traits() {
   });
 
   traits.set_supported_fan_modes({
-      climate::CLIMATE_FAN_AUTO,
-      climate::CLIMATE_FAN_LOW,
-      climate::CLIMATE_FAN_MEDIUM,
-      climate::CLIMATE_FAN_HIGH
+    climate::CLIMATE_FAN_AUTO,
+    climate::CLIMATE_FAN_LOW,
+    climate::CLIMATE_FAN_MEDIUM,
+    climate::CLIMATE_FAN_HIGH
   });
 
-  // traits.set_supported_swing_modes(this->supported_swing_modes_);
   traits.set_supports_current_temperature(true);
   traits.set_supports_two_point_target_temperature(false);
-
   traits.set_supported_presets(this->supported_presets_);
-
   traits.add_supported_preset(climate::CLIMATE_PRESET_NONE);
   traits.add_supported_preset(climate::CLIMATE_PRESET_BOOST);
-  // traits.add_supported_preset(climate::CLIMATE_PRESET_SLEEP);
 
   return traits;
 }
 
 void GreeClimate::read_state_(const uint8_t *data, uint8_t size) {
-  // get checksum byte from received data (using the last byte)
-  uint8_t data_crc = data[size-1];
-  // get checksum byte based on received data (calculating)
-  uint8_t get_crc = get_checksum_(data, size);
-
-  if (data_crc != get_crc) {
-    ESP_LOGW(TAG, "Invalid checksum.");
+  if (size < 10) {
+    ESP_LOGW(TAG, "Invalid packet (too short): %d bytes", size);
     return;
   }
 
-// now we are using only packets with 0x31 as first data byte
+  uint8_t calc_crc = get_checksum_(data, size);
+  uint8_t recv_crc = data[size - 1];
+  if (calc_crc != recv_crc) {
+    ESP_LOGW(TAG, "Checksum mismatch: calc=0x%02X recv=0x%02X", calc_crc, recv_crc);
+    dump_message_("Bad packet", data, size);
+    return;
+  }
+
   if (data[3] != 49) {
     ESP_LOGW(TAG, "Invalid packet type.");
     return;
   }
 
-  this->target_temperature = data[TEMPERATURE] / 16 + MIN_VALID_TEMPERATURE;
-  this->current_temperature = data[INDOOR_TEMPERATURE] - 40; // check later?
+  bool new_light_state = data[10] & 0x02;
+  this->display_light_state_ = new_light_state;
 
-  // partially saving current state to previous request
+  this->target_temperature = data[TEMPERATURE] / 16 + MIN_VALID_TEMPERATURE;
+  this->current_temperature = data[INDOOR_TEMPERATURE] - 40;
+
   data_write_[MODE] = data[MODE];
-  // add target temperature state too? ok
   data_write_[TEMPERATURE] = data[TEMPERATURE];
 
-  // update CLIMATE state according AC response
   switch (data[MODE] & MODE_MASK) {
-    case AC_MODE_OFF:
-      this->mode = climate::CLIMATE_MODE_OFF;
-      break;
-    case AC_MODE_AUTO:
-      this->mode = climate::CLIMATE_MODE_AUTO;
-      break;
-    case AC_MODE_COOL:
-      this->mode = climate::CLIMATE_MODE_COOL;
-      break;
-    case AC_MODE_DRY:
-      this->mode = climate::CLIMATE_MODE_DRY;
-      break;
-    case AC_MODE_FANONLY:
-      this->mode = climate::CLIMATE_MODE_FAN_ONLY;
-      break;
-    case AC_MODE_HEAT:
-      this->mode = climate::CLIMATE_MODE_HEAT;
-      break;
-    default:
-      ESP_LOGW(TAG, "Unknown AC MODE&fan: %s", data[MODE]);
+    case AC_MODE_OFF: this->mode = climate::CLIMATE_MODE_OFF; break;
+    case AC_MODE_AUTO: this->mode = climate::CLIMATE_MODE_AUTO; break;
+    case AC_MODE_COOL: this->mode = climate::CLIMATE_MODE_COOL; break;
+    case AC_MODE_DRY: this->mode = climate::CLIMATE_MODE_DRY; break;
+    case AC_MODE_FANONLY: this->mode = climate::CLIMATE_MODE_FAN_ONLY; break;
+    case AC_MODE_HEAT: this->mode = climate::CLIMATE_MODE_HEAT; break;
+    default: ESP_LOGW(TAG, "Unknown AC MODE&fan: %s", data[MODE]);
   }
 
-  // get current AC FAN SPEED from its response
   switch (data[MODE] & FAN_MASK) {
-    case AC_FAN_AUTO:
-      this->fan_mode = climate::CLIMATE_FAN_AUTO;
-      break;
-    case AC_FAN_LOW:
-      this->fan_mode = climate::CLIMATE_FAN_LOW;
-      break;
-    case AC_FAN_MEDIUM:
-      this->fan_mode = climate::CLIMATE_FAN_MEDIUM;
-      break;
-    case AC_FAN_HIGH:
-      this->fan_mode = climate::CLIMATE_FAN_HIGH;
-      break;
-    default:
-      ESP_LOGW(TAG, "Unknown AC mode&FAN: %s", data[MODE]);
+    case AC_FAN_AUTO: this->fan_mode = climate::CLIMATE_FAN_AUTO; break;
+    case AC_FAN_LOW: this->fan_mode = climate::CLIMATE_FAN_LOW; break;
+    case AC_FAN_MEDIUM: this->fan_mode = climate::CLIMATE_FAN_MEDIUM; break;
+    case AC_FAN_HIGH: this->fan_mode = climate::CLIMATE_FAN_HIGH; break;
+    default: ESP_LOGW(TAG, "Unknown AC mode&FAN: %s", data[MODE]);
   }
-
-  /*
-  switch (data[SWING]) {
-    case AC_SWING_OFF:
-      this->swing_mode = climate::CLIMATE_SWING_OFF;
-      break;
-
-    case AC_SWING_VERTICAL:
-      this->swing_mode = climate::CLIMATE_SWING_VERTICAL;
-      break;
-
-    case AC_SWING_HORIZONTAL:
-      this->swing_mode = climate::CLIMATE_SWING_HORIZONTAL;
-      break;
-
-    case AC_SWING_BOTH:
-      this->swing_mode = climate::CLIMATE_SWING_BOTH;
-      break;
-  }
-  */
 
   switch (data[10]) {
-    case 7:
-      // when COOL TURBO
-      this->preset = climate::CLIMATE_PRESET_BOOST;
-      break;
-    case 15:
-      // when HEAT TURBO
-      this->preset = climate::CLIMATE_PRESET_BOOST;
-      break;
-    default:
-      this->preset = climate::CLIMATE_PRESET_NONE;
-      break;
+    case 7: case 15: this->preset = climate::CLIMATE_PRESET_BOOST; break;
+    default: this->preset = climate::CLIMATE_PRESET_NONE; break;
   }
 
+  ESP_LOGI(TAG, "Diagnostic: data[10]=0x%02X -> display_bit=%d (reported by AC)",
+           data[10], new_light_state ? 1 : 0);
+
+  // сохраняем состояние дисплея в data_write_
+  if (this->display_light_state_) data_write_[10] |= 0x02;
+  else data_write_[10] &= ~0x02;
+
   this->publish_state();
+  this->has_valid_state_ = true;
 }
 
 void GreeClimate::control(const climate::ClimateCall &call) {
   data_write_[FORCE_UPDATE] = 175;
-  // show current temperature on display every time when sending new command. TEST!
-  data_write_[13] = 0x20;
-  
-/*
-  // logging of saved mode&fan vars
-  char str[250] = {0};
-  char *pstr = str;
-  for (int i = 0; i < sizeof(data_save_); i++) {
-    pstr += sprintf(pstr, "%02X ", data_save_[i]);
-  }
-  ESP_LOGV(TAG, "SAVED: %s", str);
-*/
 
-  // saving mode&fan values from previous 
   uint8_t new_mode = data_write_[MODE] & MODE_MASK;
   uint8_t new_fan_speed = data_write_[MODE] & FAN_MASK;
 
   if (call.get_mode().has_value()) {
     switch (call.get_mode().value()) {
-      case climate::CLIMATE_MODE_OFF:
-        new_mode = AC_MODE_OFF;
-        break;
-      case climate::CLIMATE_MODE_AUTO:
-        new_mode = AC_MODE_AUTO;
-        break;
-      case climate::CLIMATE_MODE_COOL:
-        new_mode = AC_MODE_COOL;
-        break;
-      case climate::CLIMATE_MODE_DRY:
-        new_mode = AC_MODE_DRY;
-        new_fan_speed = AC_FAN_LOW;
-        break;
-      case climate::CLIMATE_MODE_FAN_ONLY:
-        new_mode = AC_MODE_FANONLY;
-        break;
-      case climate::CLIMATE_MODE_HEAT:
-        new_mode = AC_MODE_HEAT;
-        break;
-      default:
-        ESP_LOGW(TAG, "Setting of unsupported MODE: %s", call.get_mode().value());
-        break;
+      case climate::CLIMATE_MODE_OFF: new_mode = AC_MODE_OFF; break;
+      case climate::CLIMATE_MODE_AUTO: new_mode = AC_MODE_AUTO; break;
+      case climate::CLIMATE_MODE_COOL: new_mode = AC_MODE_COOL; break;
+      case climate::CLIMATE_MODE_DRY: new_mode = AC_MODE_DRY; new_fan_speed = AC_FAN_LOW; break;
+      case climate::CLIMATE_MODE_FAN_ONLY: new_mode = AC_MODE_FANONLY; break;
+      case climate::CLIMATE_MODE_HEAT: new_mode = AC_MODE_HEAT; break;
+      default: ESP_LOGW(TAG, "Unsupported MODE"); break;
     }
   }
 
-  // set fan speed only if MODE != DRY (only LOW available)
   if (call.get_fan_mode().has_value()) {
     switch (call.get_fan_mode().value()) {
-      case climate::CLIMATE_FAN_AUTO:
-        new_fan_speed = AC_FAN_AUTO;
-        break;
-      case climate::CLIMATE_FAN_LOW:
-        new_fan_speed = AC_FAN_LOW;
-        break;
-      case climate::CLIMATE_FAN_MEDIUM:
-        new_fan_speed = AC_FAN_MEDIUM;
-        break;
-      case climate::CLIMATE_FAN_HIGH:
-        new_fan_speed = AC_FAN_HIGH;
-        break;
-      default:
-        ESP_LOGW(TAG, "Setting of unsupported FANSPEED: %s", call.get_fan_mode().value());
-        break;
+      case climate::CLIMATE_FAN_AUTO: new_fan_speed = AC_FAN_AUTO; break;
+      case climate::CLIMATE_FAN_LOW: new_fan_speed = AC_FAN_LOW; break;
+      case climate::CLIMATE_FAN_MEDIUM: new_fan_speed = AC_FAN_MEDIUM; break;
+      case climate::CLIMATE_FAN_HIGH: new_fan_speed = AC_FAN_HIGH; break;
+      default: ESP_LOGW(TAG, "Unsupported FAN SPEED"); break;
     }
   }
-  
-  // set low speed when DRY mode because other speeds are not available
-  if (new_mode == AC_MODE_DRY && new_fan_speed != AC_FAN_LOW) {
-    // ESP_LOGW(TAG, "DRY mode exception: %s", new_fan_speed);
-    new_fan_speed = AC_FAN_LOW;
-  }
+
+  if (new_mode == AC_MODE_DRY && new_fan_speed != AC_FAN_LOW) new_fan_speed = AC_FAN_LOW;
 
   if (call.get_preset().has_value()) {
     switch (call.get_preset().value()) {
       case climate::CLIMATE_PRESET_NONE:
-        if (new_mode == AC_MODE_COOL) {
-          data_write_[10] = 6;
-        } else if (new_mode == AC_MODE_HEAT) {
-          data_write_[10] = 14;
-        }
+        if (new_mode == AC_MODE_COOL) data_write_[10] = 6;
+        else if (new_mode == AC_MODE_HEAT) data_write_[10] = 14;
         break;
       case climate::CLIMATE_PRESET_BOOST:
-        if (new_mode == AC_MODE_COOL) {
-          data_write_[10] = 7;
-        } else if (new_mode == AC_MODE_HEAT) {
-          data_write_[10] = 15;
-        }
-        // skip preset when not COOL or HEAT mode
+        if (new_mode == AC_MODE_COOL) data_write_[10] = 7;
+        else if (new_mode == AC_MODE_HEAT) data_write_[10] = 15;
         break;
-      case climate::CLIMATE_PRESET_SLEEP:
-        // something
-        break;
-      default:
-        // something?
-        break;
+      default: break;
     }
   }
-
-/*
-  if (call.get_preset().value() == climate::CLIMATE_PRESET_NONE) {
-      data_[POWER] |= COMFORT_PRESET_MASK;
-    } else {
-      data_[POWER] &= ~COMFORT_PRESET_MASK;
-    }
-  }
-*/
 
   if (call.get_target_temperature().has_value()) {
-    // check if temperature set in valid limits
-    if (call.get_target_temperature().value() >= MIN_VALID_TEMPERATURE && call.get_target_temperature().value() <= MAX_VALID_TEMPERATURE)
-      data_write_[TEMPERATURE] = (call.get_target_temperature().value() - MIN_VALID_TEMPERATURE) * 16;
+    float t = call.get_target_temperature().value();
+    if (t >= MIN_VALID_TEMPERATURE && t <= MAX_VALID_TEMPERATURE)
+      data_write_[TEMPERATURE] = (t - MIN_VALID_TEMPERATURE) * 16;
   }
 
-  // temporary disabled
-  if (call.get_swing_mode().has_value()) {
-    switch (call.get_swing_mode().value()) {
-      case climate::CLIMATE_SWING_OFF:
-        // data_[SWING] = SWING_OFF;
-        break;
-      case climate::CLIMATE_SWING_VERTICAL:
-        // data_[SWING] = SWING_VERTICAL;
-        break;
-      case climate::CLIMATE_SWING_HORIZONTAL:
-        // data_[SWING] = SWING_HORIZONTAL;
-        break;
-      case climate::CLIMATE_SWING_BOTH:
-        // data_[SWING] = SWING_BOTH;
-        break;
-    }
-  }
+  if (this->display_light_state_) data_write_[10] |= 0x02;
+  else data_write_[10] &= ~0x02;
 
   data_write_[MODE] = new_mode + new_fan_speed;
-
-  // compute checksum & send data
   data_write_[CRC_WRITE] = get_checksum_(data_write_, sizeof(data_write_));
   send_data_(data_write_, sizeof(data_write_));
-
-  // change of force_update byte to "passive" state
   data_write_[FORCE_UPDATE] = 0;
 }
 
@@ -381,14 +247,16 @@ void GreeClimate::dump_message_(const char *title, const uint8_t *message, uint8
 }
 
 uint8_t GreeClimate::get_checksum_(const uint8_t *message, size_t size) {
-  // position of crc in packet
   uint8_t position = size - 1;
   uint8_t sum = 0;
-  // ignore first 2 bytes & last one
-  for (int i = 2; i < position; i++)
-    sum += message[i];
-  uint8_t crc = sum % 256;
-  return crc;
+  for (int i = 2; i < position; i++) sum += message[i];
+  return sum % 256;
+}
+
+void GreeClimate::set_display_light(bool state) {
+  this->display_light_state_ = state;
+  ESP_LOGI(TAG, "Display light set to: %s", state ? "ON" : "OFF");
+  this->control(this->make_call());
 }
 
 }  // namespace gree
