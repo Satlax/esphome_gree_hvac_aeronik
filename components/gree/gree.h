d74515674c348524406c5259347b4412635a0801
Satlax <137489486+Satlax@users.noreply.github.com>
2025-10-20 21:27:49 +0300

    Update gree.h

--- components/gree/gree.h
+++ components/gree/gree.h
@@ -2,14 +2,14 @@
 
 #include "esphome/core/component.h"
 #include "esphome/components/climate/climate.h"
+#include "esphome/components/switch/switch.h"
 #include "esphome/components/uart/uart.h"
 #include "esphome/core/log.h"
 
 namespace esphome {
 namespace gree {
 
-// AC modes
-enum ac_mode: uint8_t {
+enum ac_mode : uint8_t {
   AC_MODE_OFF = 0x10,
   AC_MODE_AUTO = 0x80,
   AC_MODE_COOL = 0x90,
@@ -18,28 +18,26 @@ enum ac_mode: uint8_t {
   AC_MODE_HEAT = 0xC0
 };
 
-// AC fan speeds
-enum ac_fan: uint8_t {
+enum ac_fan : uint8_t {
   AC_FAN_AUTO = 0x00,
   AC_FAN_LOW = 0x01,
   AC_FAN_MEDIUM = 0x02,
   AC_FAN_HIGH = 0x03
 };
 
-// Display / Sound / Turbo encodings (used as bit masks / codes)
-enum display_mode: uint8_t {
+enum display_mode : uint8_t {
   DISPLAY_OFF = 0x00,
-  DISPLAY_ON  = 0x02  // bit 1 in byte 10
+  DISPLAY_ON = 0x02
 };
 
-enum sound_mode: uint8_t {
-  SOUND_ON  = 0x00,
-  SOUND_OFF = 0x01  // bit 0 in byte 11
+enum sound_mode : uint8_t {
+  SOUND_ON = 0x00,
+  SOUND_OFF = 0x01
 };
 
-enum turbo_mode: uint8_t {
-  TURBO_OFF = 0x00,
-  TURBO_ON_CODE = 0x07 // value 7 in byte 10 indicates turbo on in some modes (we also check 15)
+enum turbo_mode : uint8_t {
+  TURBO_OFF = 0x02,
+  TURBO_ON = 0x07
 };
 
 #define GREE_START_BYTE 0x7E
@@ -66,10 +64,11 @@ class GreeClimate : public climate::Climate, public uart::UARTDevice, public Pol
   void dump_config() override;
   void control(const climate::ClimateCall &call) override;
 
-  // (optional) getters if you want to expose via lambda
-  bool is_turbo_enabled() const { return turbo_enabled_; }
-  bool is_display_enabled() const { return display_enabled_; }
-  bool is_sound_enabled() const { return sound_enabled_; }
+  void set_display(bool state);
+
+  // Свитчи для Turbo и Sound
+  switch::Switch *turbo_switch;
+  switch::Switch *sound_switch;
 
  protected:
   climate::ClimateTraits traits() override;
@@ -79,34 +78,20 @@ class GreeClimate : public climate::Climate, public uart::UARTDevice, public Pol
   uint8_t get_checksum_(const uint8_t *message, size_t size);
 
  private:
-  // base packet to send (47 bytes as in original)
-  uint8_t data_write_[47] = {
-    0x7E, 0x7E, 0x2C, 0x01,
-    0x00,0x00,0x00,0x00,  // 4..7
-    0x00,0x00,0x02,0x02,  // 8..11 (byte10 index 10 used for display/preset)
-    0x00,0x00,0x00,0x00,  // 12..15
-    0x00,0x00,0x00,0x00,  // 16..19
-    0x00,0x00,0x00,0x00,  // 20..23
-    0x00,0x00,0x00,0x00,  // 24..27
-    0x00,0x00,0x00,0x00,  // 28..31
-    0x00,0x00,0x00,0x00,  // 32..35
-    0x00,0x00,0x00,0x00,  // 36..39
-    0x00,0x00,0x00 // rest up to 47; CRC at index 46 will be set before send
-  };
+  uint8_t data_write_[47] = {0x7E, 0x7E, 0x2C, 0x01, 0x00, 0x00, 0x00, 0x00,
+                             0x00, 0x00, 0x02, 0x02, 0x00, 0x00, 0x00, 0x00,
+                             0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                             0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                             0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                             0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
   uint8_t data_read_[GREE_RX_BUFFER_SIZE] = {0};
 
   bool receiving_packet_ = false;
   bool has_valid_state_ = false;
 
-  // internal logical flags (persist in RAM)
-  bool turbo_enabled_ = false;   // Turbo ON/OFF
-  bool display_enabled_ = true;  // Display ON/OFF
-  bool sound_enabled_ = true;    // Sound ON/OFF (true = sound enabled)
-
-  // saved parsed states (for clarity)
-  display_mode parsed_display_ = DISPLAY_ON;
-  sound_mode parsed_sound_ = SOUND_ON;
-  turbo_mode parsed_turbo_ = TURBO_OFF;
+  display_mode display_state_ = DISPLAY_OFF;
+  sound_mode sound_state_ = SOUND_ON;
+  turbo_mode turbo_state_ = TURBO_OFF;
 };
 
 }  // namespace gree
