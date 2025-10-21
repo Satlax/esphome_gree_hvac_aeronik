3910cc491d2d76cd2296163a74d848b15124c7dc
Satlax <137489486+Satlax@users.noreply.github.com>
2025-10-21 12:59:36 +0300

    Update gree.h

--- components/gree/gree.h
+++ components/gree/gree.h
@@ -26,7 +26,7 @@ enum ac_fan: uint8_t {
 
 enum ac_swing: uint8_t {
   AC_SWING_OFF = 0x00,
-  AC_SWING_FULL = 0x10,
+  AC_SWING_FULL = 0x10,   // full sweep
   AC_SWING_TOP = 0x20,
   AC_SWING_MIDDLE = 0x40,
   AC_SWING_BOTTOM = 0x60
@@ -39,16 +39,14 @@ union gree_start_bytes_t {
     uint8_t u8x2[2];
 };
 
-struct gree_header_t
-{
+struct gree_header_t {
   gree_start_bytes_t start_bytes;
   uint8_t data_length;
 };
 
-struct gree_raw_packet_t
-{
+struct gree_raw_packet_t {
   gree_header_t header;
-  uint8_t data[1];
+  uint8_t data[1]; // first data byte
 };
 
 class GreeClimate : public climate::Climate, public uart::UARTDevice, public PollingComponent {
@@ -57,7 +55,10 @@ class GreeClimate : public climate::Climate, public uart::UARTDevice, public Pol
   void update() override;
   void dump_config() override;
   void control(const climate::ClimateCall &call) override;
-  
+
+  void set_supported_presets(const std::set<climate::ClimatePreset> &presets) { this->supported_presets_ = presets; }
+
+  // new public control helpers for switches
   void set_display(bool state);
   void set_turbo(bool state);
 
@@ -69,13 +70,32 @@ class GreeClimate : public climate::Climate, public uart::UARTDevice, public Pol
   uint8_t get_checksum_(const uint8_t *message, size_t size);
 
  private:
-  uint8_t data_write_[47] = {0x7E, 0x7E, 0x2C, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+  // indexes / layout constants
+  static const uint8_t FORCE_UPDATE = 7;
+  static const uint8_t MODE = 8;
+  static const uint8_t MODE_MASK = 0b11110000;
+  static const uint8_t FAN_MASK = 0b00001111;
+  static const uint8_t SWING = 12;
+  static const uint8_t CRC_WRITE = 46;
+  static const uint8_t TEMPERATURE = 9;
+  static const uint8_t INDOOR_TEMPERATURE = 46;
+
+  static const uint8_t MIN_VALID_TEMPERATURE = 16;
+  static const uint8_t MAX_VALID_TEMPERATURE = 30;
+  static const uint8_t TEMPERATURE_STEP = 1;
+
+  uint8_t data_write_[47] = {
+    0x7E,0x7E,0x2C,0x01,0x00,0x00,0x00,0x00,0x00,0x00,
+    0x02,0x02,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
+    0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
+    0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
+    0x00,0x00,0x00,0x00,0x00,0x00,0x00
+  };
   uint8_t data_read_[GREE_RX_BUFFER_SIZE] = {0};
 
   bool receiving_packet_ = false;
-  bool display_state_ = true;
-  bool turbo_state_ = false;
-  uint8_t swing_mode_ = AC_SWING_OFF;
+
+  std::set<climate::ClimatePreset> supported_presets_{};
 };
 
 }  // namespace gree
