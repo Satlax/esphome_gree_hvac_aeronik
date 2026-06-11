86fa6d85a9a4dd8d1898f66d7300f9bdd616d568
Satlax <137489486+Satlax@users.noreply.github.com>
2026-06-11 16:36:49 +0300

    Update climate.py

--- components/gree/climate.py
+++ components/gree/climate.py
@@ -2,8 +2,6 @@ import esphome.config_validation as cv
 import esphome.codegen as cg
 from esphome.components import climate, uart
 from esphome.const import CONF_ID, CONF_SUPPORTED_PRESETS
-
-# Корректный импорт перечисления ClimatePreset
 from esphome.components.climate import ClimatePreset
 
 DEPENDENCIES = ["climate", "uart"]
@@ -13,14 +11,12 @@ GreeClimate = gree_ns.class_(
     "GreeClimate", climate.Climate, cg.PollingComponent, uart.UARTDevice
 )
 
-# Используем актуальный метод climate_schema и указываем в нём наш класс
 CONFIG_SCHEMA = climate.climate_schema(GreeClimate).extend(
     {
         cv.GenerateID(): cv.declare_id(GreeClimate),
         cv.Optional(CONF_SUPPORTED_PRESETS): cv.ensure_list(
             cv.enum(
                 {
-                    # 👇 Ключевое исправление: используем атрибуты перечисления ClimatePreset
                     "NONE": ClimatePreset.CLIMATE_PRESET_NONE,
                     "BOOST": ClimatePreset.CLIMATE_PRESET_BOOST,
                     "SLEEP": ClimatePreset.CLIMATE_PRESET_SLEEP,
@@ -40,5 +36,4 @@ async def to_code(config):
     await climate.register_climate(var, config)
     await uart.register_uart_device(var, config)
     if CONF_SUPPORTED_PRESETS in config:
-        # Передаём список пресетов в C++ компонент
         cg.add(var.set_supported_presets(config[CONF_SUPPORTED_PRESETS]))
